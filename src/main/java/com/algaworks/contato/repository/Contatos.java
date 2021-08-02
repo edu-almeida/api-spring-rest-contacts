5a7d1ad011994a34ca89f18d72fc59cf5fe5b14f
Eduardo Almeida <60547531+edu-almeida@users.noreply.github.com>
2021-08-02 15:14:50 -0300

    Descritivo do arquivo

--- src/main/java/com/algaworks/contato/repository/Contatos.java
+++ src/main/java/com/algaworks/contato/repository/Contatos.java
@@ -1,3 +1,14 @@
+/*
+ *	Classe responsável em estabelecer o mapeamento objeto-relacional para
+ * objetos Java simples e comuns, em si a JPA.
+ *
+ *	IFNMG - Sistemas de Informação
+ *	Eduardo Pereira de Almeida
+ *	02/08/2021
+ *
+ *	version: 1.0.0.1
+ */
+
 package com.algaworks.contato.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
