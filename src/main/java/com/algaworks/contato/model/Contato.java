ee8d8536bdac72ffe42fdddaf612d9dc8fbdeef2
edu-almeida <edualmeida.a7@gmail.com>
2021-08-02 14:32:35 -0300

    Commit Api rest exemple com spring boot

---
+++ src/main/java/com/algaworks/contato/model/Contato.java
@@ -0,0 +1,33 @@
+package com.algaworks.contato.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+public class Contato {
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	private String sobreNome;
+
+	private String numeroCelular;
+
+	@NotNull
+	@Email
+	private String email;
+}
